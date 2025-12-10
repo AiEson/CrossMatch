59772e00c3d2bee5e3a603e596ef120eb9f88fd3
AiEson <2368442877@qq.com>
2025-12-10 11:39:10 +0800

    update training params

--- LA/code/train.sh
+++ LA/code/train.sh
@@ -1,28 +1,28 @@
 
 python train_cross_match.py \
---exp "25_75_AdamW_9000_lab4" \
---conf_thresh 0.85 \
+--exp "25_75_AdamW_14000_lab4" \
+--conf_thresh 0.75 \
 --label_num 4 \
---max_iterations 9000 \
+--max_iterations 14000 \
 --optimizer AdamW \
 --base_lr 0.001 
 
 python train_cross_match.py \
---exp "25_75_AdamW_9000_lab8" \
---conf_thresh 0.85 \
+--exp "25_75_AdamW_14000_lab8" \
+--conf_thresh 0.75 \
 --label_num 8 \
---max_iterations 9000 \
+--max_iterations 14000 \
 --optimizer AdamW \
 --base_lr 0.001 
 
 python train_cross_match.py \
---exp "25_75_AdamW_9000_lab16" \
---conf_thresh 0.85 \
+--exp "25_75_AdamW_14000_lab16" \
+--conf_thresh 0.75 \
 --label_num 16 \
---max_iterations 9000 \
+--max_iterations 14000 \
 --optimizer AdamW \
 --base_lr 0.001
 
-python test.py --model "25_75_AdamW_9000_lab4" --epoch_num 9001
-python test.py --model "25_75_AdamW_9000_lab8" --epoch_num 9001
-python test.py --model "25_75_AdamW_9000_lab16" --epoch_num 9001
+python test.py --model "25_75_AdamW_14000_lab4" --epoch_num 14001
+python test.py --model "25_75_AdamW_14000_lab8" --epoch_num 14001
+python test.py --model "25_75_AdamW_14000_lab16" --epoch_num 14001
